0c80b912459d310959fa39571fd8f98b88dfe402
Pim van den Toorn <0984004>
2022-04-07 13:11:51 +0200

    color env var added to api

--- api/buttonTrigger/index.js
+++ api/buttonTrigger/index.js
@@ -1,13 +1,22 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    // context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    // const name = (req.query.name || (req.body && req.body.name));
+    // const responseMessage = name
+    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    const color = process.env.color;
+    context.log(color);
+
+    let r = parseInt((color[0]+color[1]), 16);
+    let g = parseInt((color[2]+color[3]), 16);
+    let b = parseInt((color[4]+color[5]), 16);
 
     context.res.json({
         // status: 200, /* Defaults to 200 */
-        text: responseMessage
+        r: r,
+        g: g,
+        b: b
     });
 }
