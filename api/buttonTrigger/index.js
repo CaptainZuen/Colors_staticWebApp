587e2ae107a265b1820aad95f71730871ba6a93d
PimvdToorn <0984004@hr.nl>
2022-04-08 16:42:12 +0200

    text is bg color

--- api/buttonTrigger/index.js
+++ api/buttonTrigger/index.js
@@ -7,16 +7,10 @@ module.exports = async function (context, req) {
     //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     const color = process.env.color;
-    context.log(color);
 
-    let r = parseInt((color[0]+color[1]), 16);
-    let g = parseInt((color[2]+color[3]), 16);
-    let b = parseInt((color[4]+color[5]), 16);
 
     context.res.json({
         // status: 200, /* Defaults to 200 */
-        r: r,
-        g: g,
-        b: b
-    });
+        color: color
+    })
 }
